54db29c3fe75718cf43fbb40c16cc88bf892cb2d
Monika <Monika@192.168.0.19>
2016-06-06 01:09:23 +0200

    usuwam zbędne komentarze

--- src/gui/AutomataMainFrame.java
+++ src/gui/AutomataMainFrame.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package gui;
 
 import automata.Alphabet;
@@ -14,10 +10,6 @@ import javax.swing.AbstractAction;
 import javax.swing.Action;
 import javax.swing.DefaultListModel;
 
-/**
- *
- * @author andrzej
- */
 public class AutomataMainFrame extends javax.swing.JFrame {
 
 	// --- konstruktor
